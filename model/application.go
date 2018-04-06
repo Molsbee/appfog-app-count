6b82ffa0c9449e727bbc9a80a2453884bd603193
Molsbee <molsbee@gmail.com>
2018-04-06 09:07:09 -0500

    Refactored model slightly for clarity

--- model/application.go
+++ model/application.go
@@ -1,42 +1,5 @@
 package model
 
-type OrganizationResponse struct {
-	TotalResults int                    `json:"total_results"`
-	TotalPages   int                    `json:"total_pages"`
-	PrevURL      string                 `json:"prev_url"`
-	NextURL      string                 `json:"next_url"`
-	Resources    []OrganizationResource `json:"resources"`
-}
-
-type OrganizationResource struct {
-	Metadata Metadata           `json:"metadata"`
-	Entity   OrganizationEntity `json:"entity"`
-}
-
-type Metadata struct {
-	GUID      string `json:"guid"`
-	URL       string `json:"url"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
-
-type OrganizationEntity struct {
-	Name                     string `json:"name"`
-	BillingEnabled           bool   `json:"billing_enabled"`
-	QuotaDefinitionGUID      string `json:"quota_definition_guid"`
-	Status                   string `json:"status"`
-	QuotaDefinitionURL       string `json:"quota_definition_url"`
-	SpacesURL                string `json:"spaces_url"`
-	DomainsURL               string `json:"domains_url"`
-	PrivateDomainsURL        string `json:"private_domains_url"`
-	UsersURL                 string `json:"users_url"`
-	ManagersURL              string `json:"managers_url"`
-	BillingManagersURL       string `json:"billing_managers_url"`
-	AuditorsURL              string `json:"auditors_url"`
-	AppEventsURL             string `json:"app_events_url"`
-	SpaceQuotaDefinitionsURL string `json:"space_quota_definitions_url"`
-}
-
 type ApplicationsResponse struct {
 	TotalResults int                   `json:"total_results"`
 	TotalPages   int                   `json:"total_pages"`
